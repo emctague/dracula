fa33ca43964114efc3bf02f949cc37790d253363
Ethan McTague <mctague@blinkenshell.org>
2017-02-14 14:44:40 -0500

    Keybindings

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 This is a simple counter program, available at
 [tague.me/dracula](http://tague.me/dracula).
+
+## Keybindings
+
+The keybindings are not documented on the webpage, but still exist:
+
+|Action|Keys|
+|------|----|
+|Decrement|`down`, `enter`, `space`, `-`|
+|Increment|`up`, `+`, `=`|
+|Reset|`r`, `escape`|
+|Start|`enter`|
+
--- dracula.js
+++ dracula.js
@@ -21,27 +21,51 @@ function runCounter () {
     localStorage.counter = counter;
   }
 
-  dom.counter.onclick = function () {
+  function decreaseCount () {
     updateCounter (-1);
-  };
+  }
+  dom.counter.onclick = decreaseCount;
 
-  dom.increase.onclick = function () {
+  function increaseCount () {
     updateCounter (1);
   };
+  dom.increase.onclick = increaseCount;
 
-  dom.reset.onclick = function () {
+  function resetCount () {
     dom.setup.value = counter;
     runSetup();
+  };
+  dom.reset.onclick = resetCount;
+
+  // UI Keybindings.
+  window.onkeydown = function (e) {
+    // space, enter, minus, down keys.
+    if (e.keyCode == 32 || e.keyCode == 13 || e.keyCode == 173
+        || e.keyCode == 40)
+      decreaseCount();
+    // plus/equals, up keys.
+    else if (e.keyCode == 61 || e.keyCode == 38)
+      increaseCount();
+    // esc, r keys.
+    else if (e.keyCode == 27 || e.keyCode == 82)
+      resetCount();
   };
 }
 
 function runSetup () {
   document.body.className = "setup";
 
-  dom.start.onclick = function () {
+  function startCount () {
     localStorage.counter = dom.setup.value;
     runCounter();
-  }
+  };
+  dom.start.onclick = startCount;
+
+  // Enter key keybinding.
+  window.onkeydown = function (e) {
+    if (e.keyCode == 13)
+      startCount();
+  };
 }
 
 if (localStorage.counter) runCounter();
